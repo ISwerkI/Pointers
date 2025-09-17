8d4dde9bc48df147a5a0ade16cbac3f2fe30f1a7
Danil <Danil@DESKTOP-99KGHPD>
2025-09-17 16:59:09 +1000

    pop_back DONE

--- DynamicMemory/DynamicMemory/Source.cpp
+++ DynamicMemory/DynamicMemory/Source.cpp
@@ -7,6 +7,8 @@ void Print(int arr[], const int n);
 int* push_back(int arr[], int value, int& n);
 int* push_front(int arr[], int value, int& n);
 int* insert(int arr[], int value, int& n, int index);
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
 
 void main()
 {
@@ -19,7 +21,7 @@ void main()
 	Print(arr, n);
 	int value;
 	cout << "Введите элемент: "; cin >> value;
-	cout << "Какие действия вы хотите провернуть с массивом?" << endl << "0. push_back" << endl << "1. push_front" << endl << "2. insert" << endl;
+	cout << "Какие действия вы хотите провернуть с массивом?" << endl << "0. push_back" << endl << "1. push_front" << endl << "2. insert" << endl<<"3. pop_front"<<endl<<"4. pop_back"<<endl;
 	cin >> user_num;
 	if (user_num == 0)
 	{
@@ -33,7 +35,15 @@ void main()
 	{
 		int index;
 		cout << "Введите индекс по которому хотите расположить значение: "; cin >> index;
-		arr = insert(arr, value, n,index);
+		arr = insert(arr, value, n, index);
+	}
+	else if (user_num == 3)
+	{
+		arr = pop_front(arr, n);
+	}
+	else if (user_num == 4)
+	{
+		arr = pop_back(arr, n);
 	}
 	else
 	{
@@ -102,5 +112,30 @@ int* insert(int arr[], int value, int& n,int index)
 	delete arr;
 	arr = buffer;
 	n++;
+	return arr;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n-1; i++)
+	{
+		buffer[i] = arr[i+1];
+	}
+	delete arr;
+	arr = buffer;
+	n--;
+	return arr;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[n - 1];
+	for (int i = 0; i < n-1; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete arr;
+	arr = buffer;
+	n--;
 	return arr;
 }
